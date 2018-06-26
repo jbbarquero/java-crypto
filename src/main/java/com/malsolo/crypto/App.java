fe4641dc3ba320da8f25866f573ab18b45304683
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-06-26 11:15:46 +0200

    begin-java-crypto: java 8

--- src/main/java/com/malsolo/crypto/App.java
+++ src/main/java/com/malsolo/crypto/App.java
@@ -1,13 +1,19 @@
 package com.malsolo.crypto;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .filter(n -> n % 2 == 0)
+                .map(String::valueOf)
+                .forEach(System.out::println);
+
         System.out.println( "Hello World!" );
     }
 }
