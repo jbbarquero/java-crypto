6c320d353dc40c4ddb7cd649403522d41da5a135
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-06-26 12:05:11 +0200

    begin-java-crypto: BC Providere test.

---
+++ src/test/java/com/malsolo/crypto/ProviderTest.java
@@ -0,0 +1,34 @@
+package com.malsolo.crypto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+
+public class ProviderTest {
+
+    @BeforeClass
+    public static void installBCProvider() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    @Test
+    public void testPrecedence() throws NoSuchPaddingException, NoSuchAlgorithmException,
+            NoSuchProviderException {
+        Cipher cipher = Cipher.getInstance("Blowfish/ECB/NoPadding");
+        assertThat(cipher).isNotNull();
+        assertThat(cipher.getProvider().getName()).contains("SunJCE"); //SunJCE version 1.8
+
+        cipher = Cipher.getInstance("Blowfish/ECB/NoPadding", "BC");
+        assertThat(cipher).isNotNull();
+        assertThat(cipher.getProvider().getName()).startsWith("BC"); //BC version 1.59
+    }
+
+}
