5d8585b39fede38d8bcc8f2133765b2689450114
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-07-24 15:35:31 +0200

    begin-java-crypto: creating a certificate from a certificate request with BC 1.59.
    It's working! :)

---
+++ src/main/java/com/malsolo/crypto/certificates/PKCS10CertRequestWithBouncyCastle160Example.java
@@ -0,0 +1,166 @@
+package com.malsolo.crypto.certificates;
+
+import com.malsolo.crypto.util.Utils;
+import org.bouncycastle.asn1.pkcs.Attribute;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.Extensions;
+import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.cert.CertIOException;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v1CertificateBuilder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.*;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.ContentVerifierProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.operator.jcajce.JcaContentVerifierProviderBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCSException;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+import static com.malsolo.crypto.book.tls.Utils2.calculateDate;
+import static com.malsolo.crypto.book.tls.Utils2.calculateSerialNumber;
+
+public class PKCS10CertRequestWithBouncyCastle160Example {
+
+    private static PKCS10CertificationRequest clientCertificationRequest() throws Exception {
+        return PKCS10CertRequestExample.createPKCS10WithExtensions(Utils.generateRSAKeyPair(), "SHA256withRSA");
+    }
+
+    private static X509Certificate[] caBuildCertificateChainFromRequest(PKCS10CertificationRequest request, X509Certificate rootCert, String signatureAlgorithm, PrivateKey privateKey) throws Exception {
+        // validate the certification request
+        if (!isValidPKCS10Request(request)) {
+            System.out.println("request failed to verify!");
+            System.exit(1);
+        }
+
+        PublicKey requestPublicKey = new JcaPEMKeyConverter().getPublicKey(request.getSubjectPublicKeyInfo());
+
+        X509v3CertificateBuilder certificateBuilder = new JcaX509v3CertificateBuilder(
+                request.getSubject(),
+                calculateSerialNumber(),
+                calculateDate(0),
+                calculateDate(24 * 31),
+                new JcaX509CertificateHolder(rootCert).getSubject(),
+                requestPublicKey);
+
+        JcaX509ExtensionUtils extUtils = new JcaX509ExtensionUtils();
+
+        certificateBuilder
+                .addExtension(Extension.authorityKeyIdentifier,
+                        false, extUtils.createAuthorityKeyIdentifier(rootCert))
+                .addExtension(Extension.subjectKeyIdentifier,
+                        false, extUtils.createSubjectKeyIdentifier(request.getSubjectPublicKeyInfo()))
+                .addExtension(Extension.basicConstraints,
+                        true, new BasicConstraints(false))
+                .addExtension(Extension.keyUsage,
+                        true, new KeyUsage(KeyUsage.digitalSignature
+                                | KeyUsage.keyCertSign
+                                | KeyUsage.cRLSign));
+
+        Consumer<Attribute> processExtensionRequest = (attribute) -> {
+            if (attribute.getAttrType().equals(PKCSObjectIdentifiers.pkcs_9_at_extensionRequest)) {
+                Extensions extensions = Extensions.getInstance(attribute.getAttrValues().getObjectAt(0));
+
+                Arrays.stream(extensions.getExtensionOIDs()).forEach(
+                        asn1ObjectIdentifier -> {
+                            Extension extension = extensions.getExtension(asn1ObjectIdentifier);
+                            try {
+                                certificateBuilder.addExtension(asn1ObjectIdentifier, extension.isCritical(), extension.getExtnValue().getOctets());
+                            } catch (CertIOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+            }
+        };
+        Arrays.stream(request.getAttributes()).forEach(processExtensionRequest);
+
+        ContentSigner signer = new JcaContentSignerBuilder(signatureAlgorithm).setProvider("BC").build(privateKey);
+
+        X509CertificateHolder x509CertificateHolder = certificateBuilder.build(signer);
+
+        X509Certificate certificateFromRequest = new JcaX509CertificateConverter().setProvider("BC").getCertificate(x509CertificateHolder);
+
+        return new X509Certificate[]{certificateFromRequest, rootCert};
+    }
+
+    private static boolean isValidPKCS10Request(PKCS10CertificationRequest pkcs10Request)
+            throws OperatorCreationException, PKCSException {
+        ContentVerifierProvider verifierProvider = new JcaContentVerifierProviderBuilder()
+                .setProvider("BC").build(pkcs10Request.getSubjectPublicKeyInfo());
+
+        return pkcs10Request.isSignatureValid(verifierProvider);
+    }
+
+    private static void caSendCertificateChainToClient(X509Certificate[] certificateChain) throws IOException {
+        JcaPEMWriter pemWriter = new JcaPEMWriter(new OutputStreamWriter(System.out));
+
+        Arrays.stream(certificateChain).forEach(certificate -> {
+            try {
+                pemWriter.writeObject(certificate);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        pemWriter.close();
+    }
+
+    public static X509CertificateHolder createRootCertificate(KeyPair keyPair, String sigAlg)
+            throws OperatorCreationException {
+        X500NameBuilder x500NameBld = new X500NameBuilder(BCStyle.INSTANCE)
+                .addRDN(BCStyle.C, "ES")
+                .addRDN(BCStyle.ST, "Madrid")
+                .addRDN(BCStyle.L, "Mostoles")
+                .addRDN(BCStyle.O, "Malsolo")
+                .addRDN(BCStyle.CN, "Root Certificate");
+
+        X500Name name = x500NameBld.build();
+
+        X509v1CertificateBuilder certBldr = new JcaX509v1CertificateBuilder(
+                name,
+                calculateSerialNumber(),
+                calculateDate(0),
+                calculateDate(24 * 31),
+                name,
+                keyPair.getPublic());
+
+        ContentSigner signer = new JcaContentSignerBuilder(sigAlg)
+                .setProvider("BC").build(keyPair.getPrivate());
+
+        return certBldr.build(signer);
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Utils.installBouncyCastleProvider();
+
+        // create a root certificate
+        KeyPair rootPair = Utils.generateRSAKeyPair();
+        X509Certificate rootCert = X509CertificateGenerator.generateV1Certificate(rootPair);
+
+        X509Certificate[] buildChain = caBuildCertificateChainFromRequest(clientCertificationRequest(), rootCert, "SHA256WithRSAEncryption", rootPair.getPrivate());
+
+        caSendCertificateChainToClient(buildChain);
+
+    }
+}
