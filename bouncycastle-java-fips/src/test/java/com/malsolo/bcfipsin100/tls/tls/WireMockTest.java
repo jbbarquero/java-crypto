2460e69bc076a441500823ab7d654d696ebb7002
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-09-21 19:30:14 +0200

    begin-java-crypto: HTTPS with HttpsURLConnection, WireMock configured with TLS.

--- bouncycastle-java-fips/src/test/java/com/malsolo/bcfipsin100/tls/tls/WireMockTest.java
+++ bouncycastle-java-fips/src/test/java/com/malsolo/bcfipsin100/tls/tls/WireMockTest.java
@@ -1,24 +1,40 @@
 package com.malsolo.bcfipsin100.tls.tls;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import com.malsolo.bcfipsin100.tls.https.HTTPSClientUsingHttpsURLConnection;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static com.malsolo.bcfipsin100.pbeks.CreateKeyStores3.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class WireMockTest {
 
-    private static final int SERVER_PORT = 8089;
+    private static final String TEST_CERTS_PATH = "certsFromUtils0/";
+    private static final String KEYSTORE_PATH = TEST_CERTS_PATH + CLIENT_STORE_NAME_P12;
+    private static final String KEYSTORE_PASSWORD = new String(CLIENT_STORE_PASSWORD);
+    private static final String TRUSTSTORE_PATH = TEST_CERTS_PATH + TRUST_STORE_NAME;
+    private static final String TRUSTSTORE_PASSWORD = new String(TRUST_STORE_PASSWORD);
+
+    @ClassRule //WireMock server continue to run between test cases
+    public static WireMockClassRule wireMockClassRule = new WireMockClassRule(
+            wireMockConfig()
+                    .dynamicPort() // No-args constructor defaults to port 8080
+                    .dynamicHttpsPort()
+                    .keystorePath(KEYSTORE_PATH)
+                    .keystorePassword(KEYSTORE_PASSWORD)
+                    .keystoreType("PKCS12")
+                    .trustStorePath(TRUSTSTORE_PATH)
+                    .trustStorePassword(TRUSTSTORE_PASSWORD)
+                    .trustStoreType("JKS")
+    );
 
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig()
-            .port(SERVER_PORT) // No-args constructor defaults to port 8080
-            .dynamicHttpsPort()
-    );
+    public WireMockClassRule wireMockRule = wireMockClassRule;
 
     @Test
     public void exampleTest() throws Exception {
@@ -33,10 +49,10 @@ public class WireMockTest {
                 )
         );
 
-        System.out.printf("Dynamic HTTPs PORT %d\n", wireMockRule.httpsPort());
+        System.out.printf("HTTPS PORT: %d", wireMockRule.httpsPort());
 
         HTTPSClientUsingHttpsURLConnection client =
-                new HTTPSClientUsingHttpsURLConnection("localhost", SERVER_PORT);
+                new HTTPSClientUsingHttpsURLConnection("localhost", wireMockRule.port());
 
         String response = client.invoke(endPoint);
 
