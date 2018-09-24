2cbce7cb97d041ae51ea245f8272e7cd15513045
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-09-24 08:05:28 +0200

    begin-java-crypto: HTTPS client with HttpsURLConnection

--- bouncycastle-java-fips/src/main/java/com/malsolo/bcfipsin100/tls/https/HTTPSClientUsingHttpsURLConnection.java
+++ bouncycastle-java-fips/src/main/java/com/malsolo/bcfipsin100/tls/https/HTTPSClientUsingHttpsURLConnection.java
@@ -1,18 +1,27 @@
 package com.malsolo.bcfipsin100.tls.https;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.nio.file.Paths;
 
+import static com.malsolo.bcfipsin100.pbeks.CreateKeyStores3.*;
+import static com.malsolo.bcfipsin100.tls.HTTPSUtil.createSSLContext;
+
+@SuppressWarnings("WeakerAccess")
 public class HTTPSClientUsingHttpsURLConnection {
 
     private final String host;
     private final int port;
+    private final boolean https;
+    private final String certsPath;
 
-    public HTTPSClientUsingHttpsURLConnection(String host, int port) {
+    public HTTPSClientUsingHttpsURLConnection(String host, int port, boolean https, String certsPath) {
         this.host = host;
         this.port = port;
+        this.https = https;
+        this.certsPath = certsPath;
     }
 
     /**
@@ -21,16 +30,33 @@ public class HTTPSClientUsingHttpsURLConnection {
      * @return the response from the server as String.
      * @throws IOException if something went wrong.
      */
-    public String invoke(String endpoint) throws IOException {
+    public String invoke(String endpoint) throws Exception {
 
 
-        String server = String.format("http://%s:%d%s", host, port, endpoint);
+        String server = String.format("%s://%s:%d%s", https ? "https" : "http",
+                host, port, endpoint);
+
+        System.out.printf("Invoke server %s\n", server);
+
+        SSLContext sslContext = createSSLContext(
+                Paths.get(certsPath + CLIENT_STORE_NAME_P12).toFile(),
+                CLIENT_STORE_PASSWORD,
+                "PKCS12",
+                Paths.get(certsPath + TRUST_STORE_NAME).toFile(),
+                TRUST_STORE_PASSWORD,
+                "JKS"
+        );
+
+
         URL url = new URL(server);
 
-        URLConnection urlConnection = url.openConnection();
+        HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+        httpsURLConnection.setSSLSocketFactory(sslContext.getSocketFactory());
+        httpsURLConnection.setHostnameVerifier((host, sslSession) -> true);
+        httpsURLConnection.connect();
 
         StringBuilder sb = new StringBuilder();
-        try (InputStream bis = new BufferedInputStream(urlConnection.getInputStream())) {
+        try (InputStream bis = new BufferedInputStream(httpsURLConnection.getInputStream())) {
             Reader reader = new InputStreamReader(bis);
             int c;
             while ((c = reader.read()) != -1) {
@@ -42,6 +68,17 @@ public class HTTPSClientUsingHttpsURLConnection {
 
         return sb.toString();
 
+    }
+
+    public static void main(String[] args) throws Exception {
+        int serverPort = 8888;//9020;
+        String mainCertsPath = "bouncycastle-java-fips/certsFromUtils0/";
+
+        HTTPSClientUsingHttpsURLConnection client =
+                new HTTPSClientUsingHttpsURLConnection("localhost", serverPort,
+                        true, mainCertsPath);
+
+        System.out.println(client.invoke("/"));
     }
 
 }
